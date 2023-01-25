f654e2813773d239859671d224144c30f809feda
ruchi06 <ruchi06@gmail.com>
2023-01-25 12:15:15 +0530

    Validate abc

---
+++ RegexEmail/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RegexEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Email ID : ");
+            string email = Console.ReadLine();
+            RegexPattern.Pattern(email);
+        }
+    }
+}
---
+++ RegexEmail/RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexEmail
+{
+    internal class RegexPattern
+    {
+        public static void Pattern(string pattern)
+        {
+            Regex code = new Regex(@"(abc)");
+            bool matches = code.IsMatch(pattern);
+
+            if (matches == true)
+            {
+                Console.WriteLine("Email is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Email");
+            }
+        }
+    }
+}
